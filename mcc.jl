d95c8d685b492e8face34911cd6fa2336ff8b3f6
Claziero <lorenzoclazzer.2000@outlook.it>
2023-01-23 15:11:33 +0100

    Changed function names

--- mcc.jl
+++ mcc.jl
@@ -6,6 +6,8 @@ Constants for the MCC algorithm:
 - `ND`: Number of cells along the cuboid height enclosing the cylinder
 - `σS`: Spatial standard deviation 
 - `σD`: Directional standard deviation
+- `μψ`: Mean of the sigmoid function
+- `τψ`: Parameter of the sigmoid function
 - `ΔS`: Cell size along one direction of the cuboid base
 - `ΔD`: Cell size along the cuboid height
 """
@@ -62,7 +64,7 @@ Returns the angle in radians dϕ/dk.
 - `k::Int64`: The height of the cell.
 - `pms::Parameters`: The parameters of the algorithm.
 """
-function get_angle_from_height(k::Int64; pms::Parameters=params)::Float64
+function height2angle(k::Int64; pms::Parameters=params)::Float64
     -π + (Float64(k) - 0.5) * pms.ΔD
 end
 
@@ -75,7 +77,7 @@ with indices `(i, j)` of the minutia `m`.
 - `m::Minutia`: The minutia feature.
 - `pms::Parameters`: The parameters of the algorithm.
 """
-function get_cell_center_point(i::Int64, j::Int64, m::Minutia; pms::Parameters=params)::Point_ij
+function center(i::Int64, j::Int64, m::Minutia; pms::Parameters=params)::Point_ij
     # TODO θ in the Minutia type should be a single Float64 value instead of a Vector{Float64}
     tf = [cos(m.θ[1]) sin(m.θ[1]); -sin(m.θ[1]) cos(m.θ[1])]
     tf = tf * [Float64(i)-(pms.NS+1)/2; Float64(j)-(pms.NS+1)/2] * pms.ΔS + [m.x; m.y]
@@ -88,7 +90,7 @@ Calculate the euclidean distance between a minutia and a point.
 - `m::Minutia`: The minutia feature.
 - `p::Point`: The point.
 """
-function euclidean_distance(m::Minutia, p::Point_ij)::Float64
+function dist(m::Minutia, p::Point_ij)::Float64
     sqrt((m.x - p.x)^2 + (m.y - p.y)^2)
 end
 
@@ -100,9 +102,9 @@ represented by the point `point`.
 - `point::Point`: The point representing the cell.
 - `pms::Parameters`: The parameters of the algorithm.
 """
-function spatial_contribution_minutia(m_t::Minutia, point::Point_ij; pms::Parameters=params)::Float64
+function spatial_contribution(m_t::Minutia, point::Point_ij; pms::Parameters=params)::Float64
     # Calculate the euclidean distance between the minutia and the point
-    d = euclidean_distance(m_t, point)
+    d = dist(m_t, point)
     # Calculate the gaussian function of the euclidean distance
     exp(-d^2 / (2 * pms.σS^2)) / (sqrt(2π) * pms.σS)
 end
@@ -127,7 +129,7 @@ Compute the directional contribution of the minutia `m_t` to the minutia `m`.
 - `angle::Float64`: The angle.
 - `pms::Parameters`: The parameters of the algorithm.
 """
-function directional_contribution_minutia(m_t::Minutia, m::Minutia, angle::Float64; pms::Parameters=params)::Float64
+function directional_contribution(m_t::Minutia, m::Minutia, angle::Float64; pms::Parameters=params)::Float64
     # Calculate the directional difference between the two minutiae
     dθ = angles_difference(m.θ[1], m_t.θ[1])
     # Calculate the difference between the "angle" and the directional difference
@@ -147,7 +149,7 @@ Check if a point is valid. This function implements ξ(m, p). Returns a boolean 
 function isvalid(m::Minutia, p::Point_ij, extended_hull::Matrix; pms::Parameters=params)::Bool
     # If the euclidean distance between the minutia and the point is greater than R, 
     # then the point is invalid
-    euclidean_distance(m, p) > pms.R && return false
+    dist(m, p) > pms.R && return false
     # If point p is within the extended convex hull, then the point is valid
     extended_hull[round(Int64, p.y), round(Int64, p.x)] && return true
     # Otherwise, the point is invalid
@@ -162,9 +164,9 @@ Calculate the neighborhood of a point p of the minutia m.
 - `T::Vector{Minutia}`: The set of minutiae of the image.
 - `pms::Parameters`: The parameters of the algorithm.
 """
-function calculate_neighborhood(m::Minutia, p::Point_ij, T::Vector{Minutia}; pms::Parameters=params)::Vector{Minutia}
+function neighborhood(m::Minutia, p::Point_ij, T::Vector{Minutia}; pms::Parameters=params)::Vector{Minutia}
     # Find all minutiae different from m and whose distance is less than 3σS
-    filter(m_t -> m_t != m && euclidean_distance(m_t, p) < 3pms.σS, T)
+    filter(m_t -> m_t != m && dist(m_t, p) < 3pms.σS, T)
 end
 
 """
@@ -178,17 +180,17 @@ Calculate the entire contribution of the minutia m to the point p.
 - `pms::Parameters`: The parameters of the algorithm.
 """
 function entire_contribution_minutia(
-    m::Minutia, 
-    p::Point_ij, 
-    N_p::Vector{Minutia}, 
+    m::Minutia,
+    p::Point_ij,
+    N_p::Vector{Minutia},
     angle::Float64,
-    extended_hull::Matrix; 
+    extended_hull::Matrix;
     pms::Parameters=params)::Float64
 
     # Check if the point is valid
     isvalid(m, p, extended_hull) || return -1.0
     # Sum all the contributions of the minutiae in the neighborhood N_p of point p
-    v = sum([spatial_contribution_minutia(m_t, p; pms) * directional_contribution_minutia(m_t, m, angle; pms) for m_t in N_p])
+    v = sum([spatial_contribution(m_t, p; pms) * directional_contribution(m_t, m, angle; pms) for m_t in N_p])
     # Return the sigmoid function of the sum
     return 1 / (1 + exp(-pms.τψ * (v - pms.μψ)))
 end
