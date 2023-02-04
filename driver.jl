658e00aa8679dde37d993d7b4039487469d64951
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2023-02-04 17:02:11 +0100

    Added verification evaluation functions in driver

--- driver.jl
+++ driver.jl
@@ -4,6 +4,7 @@ using .FingerprintMatching
 using Images
 using ArgParse
 using ProgressBars
+using Plots
 
 function list_images(path)::Vector{String}
     println(path)
@@ -35,7 +36,93 @@ function compute_cylinders(img)
     cylinder_set(img, [terms; bifur])
 end
 
-posangle(x) = x > 0 ? x : x + 2π
+# Code for verification evaluation
+"""
+Function to get the list of probe's scores
+given the image path
+"""
+function get_probes_scores(img_path)
+    gray_img = Gray.(load(img_path))
+    matrix = Float32.(gray_img)
+    rows, cols = size(matrix)
+
+    @assert rows == cols
+
+    println("There are $(rows * cols - rows) probes in the matrix.")
+
+    genuine_attempts = Float32[]
+    impostor_attempts = Float32[]
+    
+    # Iterate over all coordinates of matrix
+    @inbounds for i in 1:rows
+        for j in 1:cols
+            # If the probes are the same, do not add to scores
+            i == j && continue
+
+            id1 = (i - 1) ÷ 8
+            id2 = (j - 1) ÷ 8
+
+            attempt = matrix[i, j]
+            id1 == id2 ? push!(genuine_attempts, attempt) : push!(impostor_attempts, attempt)
+        end
+    end
+
+    genuine_attempts, impostor_attempts
+end
+
+""" False Rejection Rate (FRR) """
+frr(genuine::Vector{Float32}, threshold) = count(genuine .<= threshold) / length(genuine) 
+""" False Acceptance Rate (FAR) """
+far(impostor::Vector{Float32}, threshold) = count(impostor .>= threshold) / length(impostor)
+""" Genuine Acceptance Rate (GAR) """
+gar(genuine::Vector{Float32}, threshold) = count(genuine .> threshold) / length(genuine)
+""" Genuine Recognition Rate (GRR) """
+grr(impostor::Vector{Float32}, threshold) = count(impostor .< threshold) / length(impostor)
+
+# Equations
+# FRR = 1 - GAR <=> FRR + GAR = 1
+# FAR = 1 - GRR <=> FAR + GRR = 1
+
+""" Impostor/genuine Distributions """
+function score_distributions(genuine::Vector{Float32}, impostor::Vector{Float32})
+    histogram(xlabel="Similarity Score", ylabel="Frequency", title="Score Distributions", legend=:topright)
+    histogram!(impostor, bins=500, label="Impostor", normalize=:probability, color=:red, alpha=0.5)
+    histogram!(genuine, bins=500, label="Genuine", normalize=:probability, color=:blue, alpha=0.3)
+end
+
+""" FAR vs FRR curve """
+function far_vs_frr(genuine::Vector{Float32}, impostor::Vector{Float32})
+    thresholds = range(0.3, stop=0.8, length=100)
+    frrs = frr.(Ref(genuine), thresholds)
+    fars = far.(Ref(impostor), thresholds)
+
+    # Find the equal error rate
+    index = findmin(abs.(frrs .- fars))[2]
+    eer = (frrs[index] + fars[index]) / 2
+    eer = round(100eer, digits=2)
+    println("Equal Error Rate: $eer%")
+
+    plot(xlabel="Threshold", ylabel="Rate", title="FAR vs FRR", legend=:right)
+    plot!(thresholds, 100frrs, label="FRR", color=:blue)
+    plot!(thresholds, 100fars, label="FAR", color=:red)
+end
+
+""" Receiver Operating Characteristic (ROC) curve """
+function roc_curve(genuine::Vector{Float32}, impostor::Vector{Float32})
+    thresholds = range(0.3, stop=0.8, length=100)
+    fars = far.(Ref(impostor), thresholds)
+    gars = gar.(Ref(genuine), thresholds)
+
+    # Compute the AUC
+    auc = sum(diff(fars) .* (gars[2:end] .+ gars[1:end-1])) / 2
+    auc = -round(100auc, digits=2)
+    println("Area Under Curve: $auc%")
+
+    plot(xlabel="FAR", ylabel="GAR", legend=:right, xlim=(-0.04, 1), ylim=(0, 1), title="ROC Curve")
+    plot!(fars, gars, fillrange=zero(fars), fillalpha=0.3, label="ROC", color=:blue, aspect_ratio=1)
+    plot!(0:0.5:1, 0:0.5:1, label="Random", color=:red, linestyle=:dash)
+end
+
 
 
 parse_settings = ArgParseSettings()
@@ -126,6 +213,22 @@ elseif command == "match"
 
     # Save the similarity matrix as an image
     save(output_img_paths, similarity_mat)
+elseif command == "verification"
+    input_similarity_matrix = parsed_args["input"]
+    println("Loading the similarity matrix from '$(parsed_args["input"])'")
+    output_img_path = parsed_args["output"]
+    println("Saving the curves to '$(parsed_args["output"])'")
+
+    genuine, impostor = get_probes_scores(input_similarity_matrix)
+
+    curve = far_vs_frr(genuine, impostor)
+    savefig(curve, output_img_path * "far_vs_frr.png")
+
+    curve = roc_curve(genuine, impostor)
+    savefig(curve, output_img_path * "roc_curve.png")
+
+    curve = score_distributions(genuine, impostor)
+    savefig(curve, output_img_path * "score_distributions.png")
 else
     println("Unknown command '$command'")
     exit(1)
